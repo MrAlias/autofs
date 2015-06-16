5e1799bb232396b9ccf0785e7a7a81bcddc8ca06
Tyler Yahn <tyler@moonshadowmobile.com>
2015-06-16 11:19:14 -0700

    Added main autofs class smoke test.

--- tests/init.pp
+++ tests/init.pp
@@ -1,12 +1,7 @@
-# The baseline for module testing used by Puppet Labs is that each manifest
-# should have a corresponding test manifest that declares that class or defined
-# type.
-#
-# Tests are then run by using puppet apply --noop (to check for compilation
-# errors and view a log of events) or by fully applying the test in a virtual
-# environment (to compare the resulting system state to the desired state).
-#
-# Learn more about module testing here:
-# http://docs.puppetlabs.com/guides/tests_smoke.html
-#
-include autofs
+class { 'autofs':
+  package_name   => 'test_autofs',
+  extra_packages => ['this_one', 'and_this_one'],
+  auto_master    => '/auto.master',
+  map_files_dir  => '/auto.master.d',
+  service_name   => 'test_service_autofs',
+}
