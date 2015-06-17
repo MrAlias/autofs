4c38b44d6b6bc5d06c48cb8076ca13965b79698e
Tyler Yahn <tyler@moonshadowmobile.com>
2015-06-17 14:58:01 -0700

    Updated the main autofs class smoke test.
    Added valid values to test on a Debian system.

--- tests/init.pp
+++ tests/init.pp
@@ -1,7 +1,7 @@
 class { 'autofs':
-  package_name   => 'test_autofs',
-  extra_packages => ['this_one', 'and_this_one'],
+  package_name   => 'autofs',
+  extra_packages => ['autofs-ldap', 'autofs-hesiod'],
   auto_master    => '/auto.master',
   map_files_dir  => '/auto.master.d',
-  service_name   => 'test_service_autofs',
+  service_name   => 'autofs',
 }
